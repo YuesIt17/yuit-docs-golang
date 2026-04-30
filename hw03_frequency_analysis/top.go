2953afe162c906420e3454215684ebd65d5fd8d0
yues.it17 <yues.it17@gmail.com>
2026-04-30 20:09:09 +0300

    feat: add brute force

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,53 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type item struct {
+	word  string
+	count int
+}
+
+func Top10(text string) []string {
+	frases := strings.Fields(text)
+	if len(frases) == 0 {
+		return nil
+	}
+
+	sort.Strings(frases)
+
+	words := make([]item, 0, len(frases))
+
+	currentWord := frases[0]
+	currentCount := 1
+	for i := 1; i < len(frases); i++ {	
+		frase := frases[i]
+		if frase != currentWord {
+			words = append(words, item{word: currentWord, count: currentCount})
+			currentCount = 1
+			currentWord = frase
+		} else {
+			currentCount++
+		}
+	}
+	words = append(words, item{word: currentWord, count: currentCount})
+
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].count != words[j].count {
+			return words[i].count > words[j].count
+		}
+		return words[i].word < words[j].word
+	})
+
+	if len(words) > 10 {
+		words = words[:10]
+	}
+
+	result := make([]string, 0, len(words))
+	for _, w := range words {
+		result = append(result, w.word)
+	}
+	return result
 }
